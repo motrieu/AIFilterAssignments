2bb9c47fee43e65f71a95c73cf3010b0d6be2231
Moritz <ninj24o@gmail.com>
2022-11-03 14:29:53 +0100

    implemented filter for files

--- FilterAssignment.py
+++ FilterAssignment.py
@@ -1,17 +1,26 @@
 import os
+from shutil import copy
 
+## CHANGE IO PATHS HERE
 input_path = "" # Folder that contains all the assignments, that were handed in
 output_path = "" # Folder the filtered assigments will be put in
 
-group_string = "" # Prefix of group i.e. "G2"
+## CHANGE SEARCH ARGUMENTS HERE
+group_string = [] # Prefix of group i.e. "G2", "2"
 name_list = [] # List of possible names in filename
-search_args = [group_string] + name_list
+search_args = group_string + name_list
 
-directory_list = os.listdir(input_path)
-for elem in directory_list:
-    if os.path.isfile(elem):
-        pass
-    else:
-        file_list = os.listdir(input_path + "\\" + elem)
-        if any(keyword in filename for filename in file_list for keyword in search_args)
+# recursive dfs function to find files and copy them to destination folder if search criteria is matched
+def file_check(path):
+    directory_list = os.listdir(path)
+    
+    for dir in directory_list:
+        new_path = path + "/" + dir
+        
+        if os.path.isfile(new_path) and any(keyword in dir.split("_") for keyword in search_args):
+                print(dir)
+                copy(new_path, output_path)
+        elif not os.path.isfile(new_path):
+            file_check(new_path)
 
+file_check(input_path)
